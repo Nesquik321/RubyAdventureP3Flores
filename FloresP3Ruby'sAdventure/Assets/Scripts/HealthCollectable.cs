ca166be3aa54240f7e9a7da5762df6f74947c771
Jorge Flores <145381154+Nesquik321@users.noreply.github.com>
2023-11-19 15:29:59 -0800

    fixed my project scripts
    after I fixed my scripts I went on and created my decorations into prefabs

--- FloresP3Ruby'sAdventure/Assets/Scripts/HealthCollectable.cs
+++ FloresP3Ruby'sAdventure/Assets/Scripts/HealthCollectable.cs
@@ -1,33 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class HealthCollectable : MonoBehaviour
 {
-    
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
-        Debug.Log("Object that entered the trigger : " +  other);
-
         if (controller != null)
         {
-            if (controller.currentHealth >= controller.maxHealth)
+            if(controller.health < controller.maxHealth)
             {
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
             }
+           
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- FloresP3Ruby'sAdventure/Assets/Scripts/RubyController.cs
+++ FloresP3Ruby'sAdventure/Assets/Scripts/RubyController.cs
@@ -6,17 +6,14 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    
     public float speed = 3.0f;
-
     public int maxHealth = 5;
-
     public int health { get { return currentHealth; } }
-    
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-    private int currentHealth;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +21,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         currentHealth = 1;
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        
     }
 
     // Update is called once per frame
@@ -47,7 +43,7 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0 , maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
